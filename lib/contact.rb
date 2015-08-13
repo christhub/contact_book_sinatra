58bc924337e7cdb027173513dfcda53fb6ccef05
Christian Lei & Tim Kellogg <student@epicodus.com>
2015-08-13 09:32:45 -0700

    Add class methods and a finished initialized contact class

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,12 +1,29 @@
 class Contact
-  attr_reader(:first_name, :last_name, :job_title, :company, :phones)
+  attr_reader(:first_name, :last_name, :job_title, :company, :id, :phones, :emails, :addresses)
+
+  @@contacts = []
 
   define_method(:initialize) do |attributes|
     @first_name  = attributes.fetch(:first_name)
     @last_name   = attributes.fetch(:last_name)
     @job_title   = attributes.fetch(:job_title)
     @company     = attributes.fetch(:company)
+    @id          = @@contacts.length().+(1)
     @phones      = []
+    @emails      = []
+    @addresses   = []
+  end
+
+  define_method(:save) do
+    @@contacts.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@contacts
+  end
+
+  define_singleton_method(:clear) do
+    @@contacts = []
   end
 
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -34,10 +34,53 @@ describe(Contact) do
     end
   end
 
+  describe('#id') do
+    it('returns the id of the contact') do
+      test_contact = Contact.new({:first_name => "Bob", :last_name => "Smith", :job_title => "CEO", :company => "McDonalds"})
+      expect(test_contact.id()).to(eq(1))
+    end
+  end
+
   describe('#phones') do
     it('returns the phone numbers of the contact') do
       test_contact = Contact.new({:first_name => "Bob", :last_name => "Smith", :job_title => "CEO", :company => "McDonalds"})
       expect(test_contact.phones()).to(eq([]))
     end
   end
+
+  describe('#emails') do
+    it('returns the emails of the contact') do
+      test_contact = Contact.new({:first_name => "Bob", :last_name => "Smith", :job_title => "CEO", :company => "McDonalds"})
+      expect(test_contact.emails()).to(eq([]))
+    end
+  end
+  describe('#addresses') do
+    it('returns the addresses of the contact') do
+      test_contact = Contact.new({:first_name => "Bob", :last_name => "Smith", :job_title => "CEO", :company => "McDonalds"})
+      expect(test_contact.addresses()).to(eq([]))
+    end
+  end
+
+  describe('.all') do
+    it('returns an empty array') do
+      test_contact = Contact.new({:first_name => "Bob", :last_name => "Smith", :job_title => "CEO", :company => "McDonalds"})
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('returns the saved array') do
+      test_contact = Contact.new({:first_name => "Bob", :last_name => "Smith", :job_title => "CEO", :company => "McDonalds"})
+      test_contact.save()
+      expect(Contact.all()).to(eq([test_contact]))
+    end
+  end
+
+  describe('.clear') do
+    it('returns the cleared array') do
+      test_contact = Contact.new({:first_name => "Bob", :last_name => "Smith", :job_title => "CEO", :company => "McDonalds"})
+      expect(Contact.clear()).to(eq([]))
+    end
+  end
+
 end
